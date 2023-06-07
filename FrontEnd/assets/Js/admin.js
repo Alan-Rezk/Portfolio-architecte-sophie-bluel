c4211fa9a17801086b5af434a4938d8256cd1598
Alan Rezk <Pro.alanrezk@gmail.com>
2023-06-07 12:37:05 +0200

    link fontawesome Js

--- FrontEnd/assets/Js/admin.js
+++ FrontEnd/assets/Js/admin.js
@@ -1,1 +1,25 @@
 //modale de la barre administrateur
+
+//je crée une fonction pour utiliser fontawesome
+
+function createModalLink (classModal, editText) {
+    //je crée un élément pour afficher l'icône et le paramétré avec les classes fontawesome
+    const editIcon = document.createElement('i')
+    editIcon.classList.add('fa-sharp', 'fa-solid', 'fa-pen-to-square')
+    const pText = document.createElement('p')
+    pText.innerHTML = editText
+    pText.innerHTML = editText
+    modalDivLink.classList.add('modal-div-link', 'js-modal')
+
+    modalDivLink.appendChild(editIcon)
+    modalDivLink.appendChild(pText)
+//création d'une div
+    const newDivTitle = document.createElement('div')
+    newDivTitle.className = classModal
+    newDivTitle.appendChild(modalDivLink)
+//ne pas oublier un return pour mettre fin 
+    return newDivTitle
+}
+
+//je peux maintenant créer l'admin barre
+
